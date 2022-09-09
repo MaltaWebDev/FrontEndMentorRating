e4862e2524453a1a64ab95753d7b1ab307f460d7
maltawebdev.com <maltawebdev@gmail.com>
2022-09-09 23:59:32 +0200

    added query selector

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.getElementsByClassName('.rating-three').styles.backgroundColor('var(--main-orange)');
 document.getElementsByClassName('.rating-three').styles.color('var(--white)');
+document.getElementsByClassName('.rating-two').styles.backgroundColor('var(--main-orange)');
 document.getElementsByClassName('.rating-two').styles.color('var(--white)');
 

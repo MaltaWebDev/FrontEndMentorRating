a9b0d312e4ce45f7c7805e63eff4153170d23b57
GussyGee <arniegirvaan@gmail.com>
2022-09-09 23:25:01 +0200

    added js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 document.getElementsByClassName('.rating-three').styles.backgroundColor('var(--main-orange)');
 document.getElementsByClassName('.rating-three').styles.color('var(--white)');
+document.getElementsByClassName('.rating-two').styles.color('var(--white)');
 

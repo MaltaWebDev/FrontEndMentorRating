b82878a1019ca6240f03c18d87906a4ad6d50504
MaltaWebDev <MaltaWebDev@gmail.com>
2022-09-18 11:28:28 +0200

    added JS

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-// "usestrict";
-// let buttonState = 0;
+const ratingStateBtn = document.querySelector(".rating-state-btn");
+const ratingStateCard = document.querySelector(".rating-state-card");
+const thankYouState = document.querySelector(".thank-you-state-card");
 
-// function updateState() {
-//   buttonState = 1;
-//   console.log(buttonState);
-// }
-
-// const ratingOne = document.getElementsByClassName("rating-one");
-// ratingOne.addEventListener("click", updateState);
-
-// if button one is clicked, change buttonState to 1
-// const click = document
-//   .getElementsByClassName("rating-one")
-//   .addEventListener("click", updateState);
-// if button two is clicked, change buttonState to 2
-// if button three is clicked, change buttonState to 3
-// if button four is clicked, change buttonState to 4
-// if button five is clicked, change buttonState to 5
-
-// if the button state is one of the above and the submit button is clicked
-// if ((buttonState === 1 || 2 || 3 || 4 || 5) && x) {
-// hide the rating state styles using display: none
-// unhide the thank you state styles
-// }
+ratingStateBtn.addEventListener("click", () => {
+  console.log("clicked");
+  // ratingStateCard.classList.add(".hidden");
+  // thankYouState.classList.remove(".hidden");
+});

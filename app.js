48dde99a902cbbf332ca859e6e116ac4c0a66d47
MaltaWebDev <MaltaWebDev@gmail.com>
2022-09-18 12:01:16 +0200

    added rest of JS

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+"usestrict";
 const ratingStateBtn = document.querySelector(".rating-state-btn");
 const ratingStateCard = document.querySelector(".rating-state-card");
 const thankYouState = document.querySelector(".thank-you-state-card");
+const ratingBtns = document.querySelectorAll(".rating");
+const score = document.querySelector(".score");
+let ratingScore = 3;
 
-ratingStateBtn.addEventListener("click", () => {
-  console.log("clicked");
-  // ratingStateCard.classList.add(".hidden");
-  // thankYouState.classList.remove(".hidden");
+ratingStateBtn.addEventListener("click", onSubmit);
+
+ratingBtns.forEach((btn) => {
+  btn.addEventListener("click", handleRatingBtnClick);
 });
+
+function onSubmit() {
+  ratingStateCard.classList.add("hidden");
+  score.textContent = ratingScore;
+  thankYouState.classList.remove("hidden");
+}
+
+function handleRatingBtnClick(event) {
+  ratingScore = event.target.textContent;
+  console.log(ratingScore);
+}

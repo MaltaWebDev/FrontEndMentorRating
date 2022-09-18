0317bdf565de7f6a4dfda35b1fcb03dfac0d4348
MaltaWebDev <MaltaWebDev@gmail.com>
2022-09-18 09:07:45 +0200

    added styles for thank you state

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-document.getElementsByClassName('.rating-three').styles.backgroundColor('var(--main-orange)');
-document.getElementsByClassName('.rating-three').styles.color('var(--white)');
-document.getElementsByClassName('.rating-two').styles.backgroundColor('var(--main-orange)');
-document.getElementsByClassName('.rating-two').styles.color('var(--white)');
+// "usestrict";
+// let buttonState = 0;
 
+// function updateState() {
+//   buttonState = 1;
+//   console.log(buttonState);
+// }
+
+// const ratingOne = document.getElementsByClassName("rating-one");
+// ratingOne.addEventListener("click", updateState);
+
+// if button one is clicked, change buttonState to 1
+// const click = document
+//   .getElementsByClassName("rating-one")
+//   .addEventListener("click", updateState);
+// if button two is clicked, change buttonState to 2
+// if button three is clicked, change buttonState to 3
+// if button four is clicked, change buttonState to 4
+// if button five is clicked, change buttonState to 5
+
+// if the button state is one of the above and the submit button is clicked
+// if ((buttonState === 1 || 2 || 3 || 4 || 5) && x) {
+// hide the rating state styles using display: none
+// unhide the thank you state styles
+// }

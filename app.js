92069564e1c2c7948fdc20012ae64400c96e0d67
MaltaWebDev <MaltaWebDev@gmail.com>
2022-11-05 08:48:12 +0100

    Fix mouseover issue
    Issue: mouse goes back to cursor over the label
    The correct behaviour is pointer over the whole rating button
    
    - add cursor: pointer to label

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-// "usestrict";
+"usestrict";
 // const submitBtn = document.querySelector(".submit-btn");
 // const ratingStateCard = document.querySelector(".rating-state-card");
 // const thankYouState = document.querySelector(".thank-you-state-card");
-// const ratingBtns = document.querySelectorAll(".rating-btn");
-// const score = document.querySelector(".score");
-// let ratingScore = 3;
-
-// // submitBtn.addEventListener("click", onSubmit);
-
-// // ratingBtns.forEach((btn) => {
-// //   btn.addEventListener("click", handleRatingBtnClick);
-// // });
-
-// // function onSubmit() {
-// //   ratingStateCard.classList.add("hidden");
-// //   score.textContent = ratingScore;
-// //   thankYouState.classList.remove("hidden");
-// // }
-
-// function handleRatingBtnClick(event) {
-//   ratingScore = event.target.textContent;
-//   console.log(ratingScore);
-// }
+const ratingBtn = document.querySelectorAll(".rating-btn");

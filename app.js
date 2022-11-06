8491e6595743e25e617e92eee612e99384d662ed
MaltaWebDev <MaltaWebDev@gmail.com>
2022-11-06 12:26:02 +0100

    Add event listener to submit btn

--- app.js
+++ app.js
@@ -3,3 +3,8 @@
 // const ratingStateCard = document.querySelector(".rating-state-card");
 // const thankYouState = document.querySelector(".thank-you-state-card");
 const ratingBtn = document.querySelectorAll(".rating-btn");
+
+submitBtn.addEventListener("click", () => {
+  const rating = document.querySelector(".rating-btn.active").value;
+  console.log(rating);
+});

5eefc70ad74c3d8941227c7a422c14f83e8eca93
MaltaWebDev <MaltaWebDev@gmail.com>
2022-11-06 14:05:01 +0100

    Add JS to capture rating and display in result
    - Add functions to get rating and insert into results page
    - Add event listener to submit-btn which calls both functions on click

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 "usestrict";
-// const submitBtn = document.querySelector(".submit-btn");
-// const ratingStateCard = document.querySelector(".rating-state-card");
-// const thankYouState = document.querySelector(".thank-you-state-card");
-const ratingBtn = document.querySelectorAll(".rating-btn");
 
-submitBtn.addEventListener("click", () => {
-  const rating = document.querySelector(".rating-btn.active").value;
-  console.log(rating);
+// get rating 1-5 from form, log it and return it
+function getRating() {
+  const formValue = document.getElementsByClassName(".rating-form").rating;
+  console.log(`Rating submitted: ${formValue}`);
+  return formValue;
+}
+
+// insert rating into inner text of elemtent with class .score
+function insertRating(rating) {
+  const score = document.getElementsByClassName(".score");
+  score.innerText = rating;
+  console.log(`Rating inserted: ${rating}`);
+}
+
+// add event listener to submit button which calls both functions
+const submitBtn = document.getElementsByClassName(".submit-btn");
+const clickedSubmit = addEventListener("click", submitBtn, () => {
+  const rating = getRating();
+  insertRating(rating);
 });

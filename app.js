1e84e9ece4cc21c9b3c9ab110c1009b06cb4d026
MaltaWebDev <MaltaWebDev@gmail.com>
2022-11-07 19:21:56 +0100

    Update JS

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 "usestrict";
 
-// get the rating 1-5
-function getRating() {
-  // convert the form to formData
-  const form = document.querySelector("rating-form");
-  const formData = new FormData(form);
-  const rating = formData.get("rating");
-  console.log(`Rating captured: ${rating}`);
-  return rating;
+const form = document.querySelector(".rating-form");
+console.log(form);
+const score = document.querySelector(".score");
+console.log(score);
+
+function updateScore(rating) {
+  score.textContent = rating;
 }
 
-// insert rating into outer text of elemtent with class .score
-function insertRating(rating) {
-  const score = document.querySelector(".score");
-  score.outerText = rating;
-  console.log(`Rating inserted: ${rating}`);
-}
-
-// add event listener to submit event of form which then calls both functions
-const formSubmit = document.querySelector(".rating-form");
-formSubmit.addEventListener("submit", (e) => {
-  console.log(e);
-  const rating = getRating();
-  insertRating(rating);
+form.addEventListener("submit", (e) => {
+  // e.preventDefault();
+  const rating = form.rating.value;
+  console.log(rating);
+  updateScore(rating);
 });
